309d7e39c63e92ca185232e742d693ca415181cf
Bashar Shoumali <basharshomaly1@hotmail.com>
2025-12-17 21:08:48 +0200

    some debugging consoles were deleted

--- server/index.js
+++ server/index.js
@@ -3,16 +3,11 @@ import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("📌 index.js loaded");
-
 const PORT = 5000;
 
 const start = async () => {
-  console.log("📌 start() function triggered");
-
   try {
     await connectDB();
-    console.log("📌 DB connected, starting server...");
 
     app.listen(PORT, () => {
       console.log(`🚀 Server listening on port ${PORT}`);
